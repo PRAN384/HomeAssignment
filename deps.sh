5ccb4e5640b5d208b151fea02fe805edb69b7725
pransh-Home-Ubuntu <pran384@aucklanduni.ac.nz>
2022-12-25 00:12:47 +1300

    Dependencies and notes being noted

---
+++ deps.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+## Mosquitto Server installation and running 
+sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
+sudo apt-get update
+sudo apt-get install mosquitto
+sudo apt-get install mosquitto-clients
+sudo apt clean
+pip3 install paho-mqtt
+
+## Need Testing 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
